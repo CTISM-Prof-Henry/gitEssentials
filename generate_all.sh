8e7807a474e458dc724cd943b443a311c0312958
henryzord <henry.cagnini@gmail.com>
2022-11-24 17:05:25 -0300

    added instruction to export path

--- generate_all.sh
+++ generate_all.sh
@@ -1,6 +1,7 @@
 mmdc -i docsource/source/fork_pull_request_diagrama.mmd -o docsource/imagens/fork_pull_request_diagrama.svg
 mmdc -i docsource/source/guia_rapido_diagrama.mmd -o docsource/imagens/guia_rapido_diagrama.svg
 make github
+export PATH=$PATH:node_modules/.bin
 make latex
 (cd _build/latex && pdflatex main.tex)
 (cd _build/latex && pdflatex main.tex)
